4539da2ab9c739d672fedd290307cf622a7aa6a6
Raphael Freire <raphaelmachadofreire@gmail.com>
2019-11-23 13:22:26 -0300

    Blog

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import * as S from "./styled"
+
+const Avatar = () => {
+  const { avatarImage } = useStaticQuery(
+    graphql`
+      query {
+        avatarImage: file(relativePath: { eq: "profile-photo.png" }) {
+          childImageSharp {
+            fluid(
+              maxWidth: 60
+              quality: 100
+            ) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return <S.AvatarWrapper fluid={avatarImage.childImageSharp.fluid} />
+}
+
+export default Avatar
